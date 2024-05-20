2bcc8c8bb919cc9dc88a53037df396b8658c8232
carrotteddy <devcarrott@gmail.com>
2024-05-20 21:43:16 +0600

    add editing notes and little change notes creating

---
+++ app/src/main/java/com/example/note_app/presentation/fragments/createNote/OnNoteDetailCallback.java
@@ -0,0 +1,5 @@
+package com.example.note_app.presentation.fragments.createNote;
+
+public interface OnNoteDetailCallback {
+	void onNoteDetailsRetrieved(String title, String text);
+}
---
+++ app/src/main/java/com/example/note_app/presentation/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.note_app.presentation.utils;
+
+public class Constants {
+	public static final String SCREEN_MODE_KEY = "screen_mode";
+	public static final String EDIT_SCREEN_MODE = "edit_mode";
+	public static final String ADD_SCREEN_MODE = "add_mode";
+	public static final String NOTE_ID_KEY = "note_id";
+}
